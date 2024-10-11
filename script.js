d592e493bf4cfe17caa391a26468a011f330afb9
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-11 13:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	const heap = new MinPriorityQueue({ priority: x => x });
+    arr.forEach(num => heap.enqueue(num));
+    
+    let totalCost = 0;
+
+	while (heap.size() > 1) {
+		let first = heap.dequeue().element;
+        let second = heap.dequeue().element;
+
+		let cost = first + second;
+        totalCost += cost;
+
+		heap.enqueue(cost);
+	}
+	return totalcost;
   
 }
 
