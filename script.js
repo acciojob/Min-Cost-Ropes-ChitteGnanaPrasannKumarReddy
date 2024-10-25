a433b0ba269b40782ac33cf17e99306f30a2bad1
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-25 19:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-const { MinPriorityQueue } = require;
+const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
 
 function mincost(arr) {
-    const heap = new MinPriorityQueue({ priority: x => x });
-    arr.forEach(num => heap.enqueue(num));
+    const heap = new MinPriorityQueue({ priority: (x) => x });
+    arr.forEach((num) => heap.enqueue(num));
     
     let totalCost = 0;
 
     while (heap.size() > 1) {
         let first = heap.dequeue().element;
         let second = heap.dequeue().element;
-
         let cost = first + second;
         totalCost += cost;
-
         heap.enqueue(cost);
     }
 
     return totalCost;
 }
 
-module.exports = mincost; 
-
-const mincost = require;
+module.exports = mincost;
 
 console.log(mincost([4, 3, 2, 6])); 
 console.log(mincost([1, 2, 3, 4, 5])); 
-
-
-
